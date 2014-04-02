aafc47022f787427d8b51f01e2e22f2ec0e07b56
Kent Holloway <Kent.Holloway@Capella.edu>
2014-04-01 22:05:37 -0500

    Clean up, REST API no longer needs the zone or partition text, just a zone or partition number

--- dscAlarmIntegrationSmarththingsApp.groovy
+++ dscAlarmIntegrationSmarththingsApp.groovy
@@ -5,22 +5,30 @@
  */
 
 preferences {
-  def events = ['alarm','closed','open','closed','partitionready','partitionnotready','partitionarmed','partitionalarm','partitionexitdelay','partitionentrydelay']
+
   section("Alarm Panel:") {
-    input "panel", "device.dSCPanel", title: "Choose Alarm Panel?", multiple: false, required: true
+    input "panel", "device.polling", title: "Alarm Panel (required)", multiple: false, required: true
   }
-  section("Activate Switches?) {
-    input "lights", "capability.switch", title: "Which lights/switches", multiple: true, required: false
+  section("Control Switches:") {
+    input "lights", "capability.switch", title: "Which lights/switches?", multiple: true, required: false
   }
-  section("Activate alarm?) {
-    input "alarms", "capability.alarm", title: "Which Alarm(s)", multiple: true, required: false
+  section("Activate Alarm Strobe:") {
+    input "alarms", "capability.alarm", title: "Which Alarm(s)?", multiple: true, required: false
   }
-  section("Notifications (optional)") {
-    input "sendPush", "enum", title: "Push Notifiation", required: false, metadata: [values: ["Yes","No"]]
+  section("Notifications (optional):") {
+    input "sendPush", "enum", title: "Push Notifiation", required: false,
+      metadata: [
+       values: ["Yes","No"]
+      ]
     input "phone1", "phone", title: "Phone Number", required: false
   }
-  section("Notification events:") {
-    input "notifyEvents", "enum", title: "Which Events?", description: "default (all)", required: false, multiple: true, options: events
+  section("Notification events (optional):") {
+    input "notifyEvents", "enum", title: "Which Events?", description: "default (none)", required: false, multiple: true,
+     options:
+      ['all','alarm','closed','open','closed','partitionready',
+       'partitionnotready','partitionarmed','partitionalarm',
+       'partitionexitdelay','partitionentrydelay'
+      ]
   }
 }
 
@@ -49,31 +57,46 @@ void updateZoneOrPartition() {
 
 private update(panel) {
     // log.debug "update, request: params: ${params} panel: ${panel.name}"
+    def zoneorpartition = params.zoneorpart
 
     // Add more events here as needed
-    // Each event maps to a command in your "Alarm Panel" device type
+    // Each event maps to a command in your "DSC Panel" device type
     def eventMap = [
-      '601':'alarm',
-      '602':'closed',
-      '609':'open',
-      '610':'closed',
-      '650':'partitionready',
-      '651':'partitionnotready',
-      '652':'partitionarmed',
-      '654':'partitionalarm',
-      '656':'partitionexitdelay',
-      '657':'partitionentrydelay'
+      '601':"zone alarm",
+      '602':"zone closed",
+      '609':"zone open",
+      '610':"zone closed",
+      '650':"partition ready",
+      '651':"partition notready",
+      '652':"partition armed",
+      '654':"partition alarm",
+      '656':"partition exitdelay",
+      '657':"partition entrydelay"
     ]
-    def zoneorpartition = params.zoneorpart
+
+    // get our passed in eventcode
     def eventCode = params.eventcode
     if (eventCode)
     {
       // Lookup our eventCode in our eventMap
-      def command = eventMap."${eventCode}"
-      if (command)
+      def opts = eventMap."${eventCode}"?.tokenize()
+      // log.debug "Options after lookup: ${opts}"
+      // log.debug "Zone or partition: $zoneorpartition"
+      if (opts[0])
       {
-        // We have a valid command, lets send it to the device
-        panel."$command"(zoneorpartition)
+        // We have some stuff to send to the device now
+        // this looks something like panel.zone("open", "1")
+        panel."${opts[0]}"("${opts[1]}", "$zoneorpartition")
       }
+    }
+}
+
+private sendMessage(msg) {
+    def newMsg = "Alarm Notification: $msg"
+    if (phone1) {
+        sendSms(phone1, newMsg)
+    }
+    if (sendPush == "Yes") {
+        sendPush(newMsg)
     }
 }
--- dscPanelDeviceSmartthings.groovy
+++ dscPanelDeviceSmartthings.groovy
@@ -1,197 +1,142 @@
-/**
+/*
  *  DSC Panel
  *
  *  Author: Kent Holloway <drizit@gmail.com>
  *  Date: 2014-03-20
  */
- // for the UI
+
+// for the UI
 metadata {
   // Automatically generated. Make future change here.
   definition (name: "DSC Panel", author: "drizit@gmail.com") {
-        // Change or define capabilities here as needed
+    // Change or define capabilities here as needed
     capability "Refresh"
-    capability "Switch"
     capability "Contact Sensor"
-    capability "Indicator"
     capability "Polling"
 
-        // Add commands as needed
-    command "open"
-    command "closed"
-        command "alarm"
-    command "partitionready"
-    command "partitionnotready"
-      command "partitionarmed"
-      command "partitionexitdelay"
-      command "partitionentrydelay"
-      command "partitionalarm"
+    // Add commands as needed
+    command "zone"
+    command "partition"
   }
 
   simulator {
     // Nothing here, you could put some testing stuff here if you like
   }
 
-    tiles {
-        // NOTE: Leave the first item "panelstatus" alone
-        // but edit all the remaining Tiles to match the zone numbers you care about
-        // You have to change both the "zone1" name and the "device.zone1" type to match your zone
-        // Then edit the details line further down in this section to match
-        // all the zone numbers you setup or the icons will be missing in your app
-        // You can add more rows if needed, just copy/paste the standardTile lines
-        // for more
-        
-        // Final note: if you add/remove/change zones you have to force quit your
-        // smartthings app on iOS (probably on Android also) to see the new tile layout
-        
-        // First Row
-        standardTile("panelstatus", "device.panelstatus", width: 2, height: 2, canChangeBackground: true, canChangeIcon: true) {
-            state "armed",     label: 'Armed',      backgroundColor: "#79b821", icon:"st.Home.home3"
-            state "exitdelay", label: 'Exit Delay', backgroundColor: "#ff9900", icon:"st.Home.home3"
-            state "entrydelay",label: 'EntryDelay', backgroundColor: "#ff9900", icon:"st.Home.home3"
-            state "open",      label: 'Open',       backgroundColor: "#ffcc00", icon:"st.Home.home2"
-            state "ready",     label: 'Ready',      backgroundColor: "#79b821", icon:"st.Home.home2"
-            state "alarm",     label: 'Alarm',      backgroundColor: "#ff0000", icon:"st.Home.home3"
-        }
-        // Top right icon
-    standardTile("zone1", "device.zone1", canChangeBackground: true, canChangeIcon: true) {
-      state "open",   label: '${name}', icon: "st.contact.contact.open",   backgroundColor: "#ffa81e"
-      state "closed", label: '${name}', icon: "st.contact.contact.closed", backgroundColor: "#79b821"
-      state "alarm",  label: '${name}', icon: "st.contact.contact.open",   backgroundColor: "#ff0000"
-        }
-        // 2nd row far right icon
-    standardTile("zone2", "device.zone2", canChangeBackground: true, canChangeIcon: true) {
-      state "open",   label: '${name}', icon: "st.contact.contact.open",   backgroundColor: "#ffa81e"
-      state "closed", label: '${name}', icon: "st.contact.contact.closed", backgroundColor: "#79b821"
-      state "alarm",  label: '${name}', icon: "st.contact.contact.open",   backgroundColor: "#ff0000"
-        }
-        // third row from left to right
-    standardTile("zone3", "device.zone3", canChangeBackground: true, canChangeIcon: true) {
-      state "open",   label: '${name}', icon: "st.contact.contact.open",   backgroundColor: "#ffa81e"
-      state "closed", label: '${name}', icon: "st.contact.contact.closed", backgroundColor: "#79b821"
-      state "alarm",  label: '${name}', icon: "st.contact.contact.open",   backgroundColor: "#ff0000"
-        }
-    standardTile("zone15", "device.zone15", canChangeBackground: true, canChangeIcon: true) {
-      state "open",   label: '${name}', icon: "st.doors.garage.garage-open",   backgroundColor: "#ffa81e"
-      state "closed", label: '${name}', icon: "st.doors.garage.garage-closed", backgroundColor: "#79b821"
-      state "alarm",  label: '${name}', icon: "st.doors.garage.garage-open",   backgroundColor: "#ff0000"
-        }
-    standardTile("zone16", "device.zone16", canChangeBackground: true, canChangeIcon: true) {
-      state "open",   label: '${name}', icon: "st.doors.garage.garage-open",   backgroundColor: "#ffa81e"
-      state "closed", label: '${name}', icon: "st.doors.garage.garage-closed", backgroundColor: "#79b821"
-      state "alarm",  label: '${name}', icon: "st.doors.garage.garage-open",   backgroundColor: "#ff0000"
-        }
- 
-       // Fourth row from left to right
-    standardTile("zone17", "device.zone17", canChangeBackground: true, canChangeIcon: true) {
-      state "open",   label: '${name}', icon: "st.contact.contact.open",   backgroundColor: "#ffa81e"
-      state "closed", label: '${name}', icon: "st.contact.contact.closed", backgroundColor: "#79b821"
-      state "alarm",  label: '${name}', icon: "st.contact.contact.open",   backgroundColor: "#ff0000"
-        }
+  tiles {
+    // Update all tiles below to match your partition numbers and zone numbers.
+    // You have to change both the "zone1" name and the "device.zone1" type to match your zone
+    // Then edit the details line further down in this section to match
+    // all the zone numbers you setup or the icons will be missing in your app
+    // You can add more rows if needed, just copy/paste the standardTile lines
+    // for more
 
-        // Fourth Row
-        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat") {
-            state "default", action:"polling.poll", icon:"st.secondary.refresh"
-        }
+    // Final note: if you add/remove/change zones you have to force quit your
+    // smartthings app on iOS (probably on Android also) to see the new tile layout after publishing your device
 
-        // This tile will be the tile that is displayed on the Hub page.
-        main "panelstatus"
-
-        // These tiles will be displayed when clicked on the device, in the order listed here.
-        details(["panelstatus", "zone1", "zone2", "zone3", "zone15", "zone16", "zone17", "refresh"])
+    // First Row
+    standardTile("partition1", "device.partition1", width: 2, height: 2, canChangeBackground: true, canChangeIcon: true) {
+      state "armed",     label: 'Armed',      backgroundColor: "#79b821", icon:"st.Home.home3"
+      state "exitdelay", label: 'Exit Delay', backgroundColor: "#ff9900", icon:"st.Home.home3"
+      state "entrydelay",label: 'EntryDelay', backgroundColor: "#ff9900", icon:"st.Home.home3"
+      state "notready",  label: 'Open',       backgroundColor: "#ffcc00", icon:"st.Home.home2"
+      state "ready",     label: 'Ready',      backgroundColor: "#79b821", icon:"st.Home.home2"
+      state "alarm",     label: 'Alarm',      backgroundColor: "#ff0000", icon:"st.Home.home3"
     }
+    // 1st row far right icon
+    standardTile("zone1", "device.zone1", canChangeBackground: true, canChangeIcon: true) {
+      state "open",   label: 'Frnt Door', icon: "st.contact.contact.open",   backgroundColor: "#ffa81e"
+      state "closed", label: 'Frnt Door', icon: "st.contact.contact.closed", backgroundColor: "#79b821"
+      state "alarm",  label: 'Frnt Door', icon: "st.contact.contact.open",   backgroundColor: "#ff0000"
+    }
+    // 2nd row far right icon
+    standardTile("zone2", "device.zone2", canChangeBackground: true, canChangeIcon: true) {
+      state "open",   label: 'GR Entry', icon: "st.contact.contact.open",   backgroundColor: "#ffa81e"
+      state "closed", label: 'GR Entry', icon: "st.contact.contact.closed", backgroundColor: "#79b821"
+      state "alarm",  label: 'GR Entry', icon: "st.contact.contact.open",   backgroundColor: "#ff0000"
+    }
+    // third row from left to right
+    standardTile("zone3", "device.zone3", canChangeBackground: true, canChangeIcon: true) {
+      state "open",   label: 'GR Service', icon: "st.contact.contact.open",   backgroundColor: "#ffa81e"
+      state "closed", label: 'GR Service', icon: "st.contact.contact.closed", backgroundColor: "#79b821"
+      state "alarm",  label: 'GR Service', icon: "st.contact.contact.open",   backgroundColor: "#ff0000"
+    }
+    standardTile("zone15", "device.zone15", canChangeBackground: true, canChangeIcon: true) {
+      state "open",   label: 'Garage1', icon: "st.doors.garage.garage-open",   backgroundColor: "#ffa81e"
+      state "closed", label: 'Garage1', icon: "st.doors.garage.garage-closed", backgroundColor: "#79b821"
+      state "alarm",  label: 'Garage1', icon: "st.doors.garage.garage-open",   backgroundColor: "#ff0000"
+    }
+    standardTile("zone16", "device.zone16", canChangeBackground: true, canChangeIcon: true) {
+      state "open",   label: 'Garage2', icon: "st.doors.garage.garage-open",   backgroundColor: "#ffa81e"
+      state "closed", label: 'Garage2', icon: "st.doors.garage.garage-closed", backgroundColor: "#79b821"
+      state "alarm",  label: 'Garage2', icon: "st.doors.garage.garage-open",   backgroundColor: "#ff0000"
+    }
+
+    // Fourth row from left to right
+    standardTile("zone17", "device.zone17", canChangeBackground: true, canChangeIcon: true) {
+      state "open",   label: 'Sliding', icon: "st.contact.contact.open",   backgroundColor: "#ffa81e"
+      state "closed", label: 'Sliding', icon: "st.contact.contact.closed", backgroundColor: "#79b821"
+      state "alarm",  label: 'Sliding', icon: "st.contact.contact.open",   backgroundColor: "#ff0000"
+    }
+    standardTile("zone18", "device.zone18", canChangeBackground: true, canChangeIcon: true) {
+      state "open",   label: 'Glass', icon: "st.contact.contact.open",   backgroundColor: "#ffa81e"
+      state "closed", label: 'Glass', icon: "st.contact.contact.closed", backgroundColor: "#79b821"
+      state "alarm",  label: 'Glass', icon: "st.contact.contact.open",   backgroundColor: "#ff0000"
+    }
+    // BlankTile that does nothing, just cleans up this row
+    standardTile("blanktile", "device.blanktile", width: 1, height: 1, decoration: "flat") {
+      state "blank",  label: "", icon: ""
+    }
+
+    // Fifth Row
+    // Not used any more..
+    /*
+      standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat") {
+        state "default", action:"polling.poll", icon:"st.secondary.refresh"
+      }
+    */
+
+    // This tile will be the tile that is displayed on the Hub page.
+    main "partition1"
+
+    // These tiles will be displayed when clicked on the device, in the order listed here.
+    details(["partition1", "zone1", "zone2", "zone3", "zone15", "zone16", "zone17", "zone18", "blanktile"])
+  }
 }
 
 // parse events into attributes
 def parse(String description) {
   // log.debug "Parsing '${description}'"
-  // TODO: handle 'switch' attribute
-  // TODO: handle 'indicatorStatus' attribute
-  // TODO: handle '' attribute
-    def myValues = description.tokenize()
-    // log.debug "Description: ${myValues[0]} - ${myValues[1]}"
-    sendEvent (name: "${myValues[0]}", value: "${myValues[1]}")
+  def myValues = description.tokenize()
+
+  // log.debug "Description: ${myValues[0]} - ${myValues[1]}"
+  sendEvent (name: "${myValues[0]}", value: "${myValues[1]}")
 }
 
 // handle commands
-def open(String zone) {
-    // log.debug "In Open function for Zone: $zone"
-    sendEvent (name: "${zone}", value: 'open')
+def zone(String state, String zone) {
+  // state will be a valid state for a zone (open, closed, alarm)
+  // zone will be a number for the zone
+  log.debug "Zone: ${state} for zone: ${zone}"
+  sendEvent (name: "zone${zone}", value: "${state}")
 }
 
-def closed(String zone) {
-    // log.debug "In Closed function for Zone: $zone"
-    sendEvent (name: "${zone}", value: 'closed')
-}
-
-def alarm(String zone) {
-    // log.debug "Alarm for Zone: $zone"
-    sendEvent (name: "${zone}", value: 'alarm')
-}
-
-def partitionready(String partition) {
-    // log.debug "Partition ready: $partition"
-    sendEvent (name: "panelstatus", value: 'ready')
-}
-
-def partitionnotready(String partition) {
-    // log.debug "Partition not ready: $partition"
-    sendEvent (name: "panelstatus", value: 'open')
-}
-
-def partitionarmed(String partition) {
-    // log.debug "Partition Armed: $partition"
-    sendEvent (name: "panelstatus", value: 'armed')
-}
-
-def partitionexitdelay(String partition) {
-    // log.debug "Partition Exit Delay: $partition"
-    sendEvent (name: "panelstatus", value: 'exitdelay')
-}
-
-def partitionentrydelay(String partition) {
-    // log.debug "Partition Entry Delay: $partition"
-    sendEvent (name: "panelstatus", value: 'entrydelay')
-}
-
-def partitionalarm(String partition) {
-    // log.debug "Partition Alarm: $partition"
-    sendEvent (name: "panelstatus", value: 'alarm')
-}
-
-def on() {
-  log.debug "Executing 'on'"
-  // TODO: handle 'on' command
-}
-
-def off() {
-  log.debug "Executing 'off'"
-  // TODO: handle 'off' command
+def partition(String state, String partition) {
+  // state will be a valid state for the panel (ready, notready, armed, etc)
+  // partition will be a partition number, for most users this will always be 1
+  log.debug "Partition: ${state} for partition: ${partition}"
+  sendEvent (name: "partition${partition}", value: "${state}")
 }
 
 def poll() {
   log.debug "Executing 'poll'"
   // TODO: handle 'poll' command
-    // On poll what should we do? nothing for now..
-}
-
-def indicatorWhenOn() {
-  log.debug "Executing 'indicatorWhenOn'"
-  // TODO: handle 'indicatorWhenOn' command
-}
-
-def indicatorWhenOff() {
-  log.debug "Executing 'indicatorWhenOff'"
-  // TODO: handle 'indicatorWhenOff' command
-}
-
-def indicatorNever() {
-  log.debug "Executing 'indicatorNever'"
-  // TODO: handle 'indicatorNever' command
+  // On poll what should we do? nothing for now..
 }
 
 def refresh() {
   log.debug "Executing 'refresh' which is actually poll()"
-    poll()
+  poll()
   // TODO: handle 'refresh' command
 }
 
